8dbbc88b87c646838c8c80855d575dbb5c8ea6e7
Nikolayz <zans55@yandex.ru>
2023-02-08 23:30:35 +0300

    1

---
+++ 31/Program.cs
@@ -0,0 +1,13 @@
+﻿// Вывести на экран кубы чисел от 1 до N
+int a=1;
+double b;
+
+double Cube(int N)
+{while (a<=N)
+{ b=Math.Pow(a,3);
+System.Console.WriteLine($"{b}"); // не знаю как вывести за пределы цикла, чтобы не снизили оценку
+a=a+1;
+}
+return a;
+}
+Cube(10);
