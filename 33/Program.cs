4e769d68cefdb225cd3d58669498bc36e9e79ea3
Nikolayz <zans55@yandex.ru>
2023-02-09 14:28:17 +0300

    33

---
+++ 33/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+Double C=0;
+double Method(double A,double B)
+{ 
+while (A>0)
+ { C=Math.Pow(A,B);
+ break;
+ }
+ return C;
+}
+Console.WriteLine(Method(5,8));
