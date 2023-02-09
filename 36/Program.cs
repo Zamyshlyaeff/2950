a8a70d72a61622b2439d6dd147a027781d931d70
Nikolayz <zans55@yandex.ru>
2023-02-09 16:14:18 +0300

    36

---
+++ 36/Program.cs
@@ -0,0 +1,13 @@
+﻿// Написать программу вычисления произведения чисел от 1 до N
+int A=0; int C=1;
+int Method(int N)
+{ for (C=1;C<=N;C++)
+    { int y;
+     for (y=1;y<=N;y++)
+     {  A=C*y;
+        Console.WriteLine($"{C}*{y}={A}");
+     }
+    }
+   return A;
+}
+Method(15);
