df6acb4925d8dc3014a894ee0059ac266bba4209
Nikolayz <zans55@yandex.ru>
2023-02-09 14:40:44 +0300

    35

---
+++ 35/Program.cs
@@ -0,0 +1,11 @@
+﻿// Подсчитать сумму цифр в числе. Сделать подпрограмму.
+int A=0;
+int Summ(int N)
+{  while (N!=0)
+   { 
+     A=A+N%10;
+     N=N/10;
+   }
+   return A;
+}
+Console.WriteLine(Summ(23457));
